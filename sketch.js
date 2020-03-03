70ca2f60dcd15d3aa6b51a507745675e0cafa4ac
FluffyBaloo <michael.a.rodriguez@sjsu.edu>
2020-03-02 18:47:58 -0800

    commit p5 editor files

---
+++ sketch.js
@@ -0,0 +1,24 @@
+//random words
+let words = ["Pickles","Dabba", "Nee-Huh", "Egg", "wawa","Da Nope"];
+
+  function setup() {
+    createCanvas(600, 600);
+    background(200);
+
+
+console.log("initial array is")
+console.log(words);
+
+console.log("array after pop")
+words.pop();
+console.log(words);
+
+words.push("temperate")
+
+
+  }
+
+  function draw() {
+
+
+  }
