4bba940040df3bd339e7a1be7118dcb48d072e20
FluffyBaloo <32744572+FluffyBaloo@users.noreply.github.com>
2020-03-11 17:34:53 -0700

    commit

--- sketch.js
+++ sketch.js
@@ -1,24 +1,58 @@
-//random words
-let words = ["Pickles","Dabba", "Nee-Huh", "Egg", "wawa","Da Nope"];
+//random words I Say everyday
+let words = [{
+  name: "Pickles",
+  meaning: "damn"
+}, {
+  name: "Dabba",
+  meaning: "stop it"
+}, {
+  name: "Nee-Huh",
+  meaning: "yeah with an uh huh",
+}, {
+  name: "Egg",
+  meaning: "nickname"
+} , {
+  name: "wawa",
+  meaning: "water"
+} , {
+  name: "Da Nope",
+  meaning: "no"
+}];
 
-  function setup() {
-    createCanvas(600, 600);
-    background(200);
+let randomIndex;
+let animating = false;
 
+function setup() {
+  createCanvas(600, 600);
+  background(200);
+  textSize(32);
 
-console.log("initial array is")
-console.log(words);
+text("click to randomize", 50 , 50);
+}
 
-console.log("array after pop")
-words.pop();
-console.log(words);
+function draw() {
 
-words.push("temperate")
-
-
+  if(animating == true){
+    square(random(width), random(height), random(50, 200));
   }
+}
 
-  function draw() {
+function randomizer(){
+    animating = false;
 
+    if (words[0]) {
+    background(random(200, 255));
+    randomIndex = int(random(words.length));
+    text(words[randomIndex].name, 50, 50);
+    words.splice(randomIndex, 1);
+   } else {
+    text("nothing left!", 50, 50);
+   }
 
-  }
+}
+
+function mousePressed() {
+  animating = true;
+  setTimeout(randomizer, 2000);
+
+}
